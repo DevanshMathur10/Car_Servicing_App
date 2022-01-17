45638d24a4608e70292d2449986ad1d0347816c8
Devansh Mathur <93263435+DevanshMathur10@users.noreply.github.com>
2022-01-17 18:17:22 +0530

    Update loginwindow.py

--- loginwindow.py
+++ loginwindow.py
@@ -103,7 +103,7 @@ def submitpass():
     if pass_box.get()==p:
         passshow=Label(frame,text=" CORRECT OTP ",background="#2E0063",fg='white', font=('arial', 9))
         passshow.grid(row=4,column=0,columnspan=2,padx=8)
-        servicingwindow()
+        servicingwindow(str(email_box.get()))
     else:
         passshow=Label(frame,text="INCORRECT OTP",background="#2E0063",fg='white', font=('arial', 9))
         passshow.grid(row=4,column=0,columnspan=2,padx=8)
